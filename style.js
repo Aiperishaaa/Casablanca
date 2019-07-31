6f6025ad25c6abb52f267fbc1d1effc0a7216705
Aiperishaaa <aiperishaaa@gmail.com>
2019-07-31 22:57:42 +0600

    Add first part page

---
+++ style.js
@@ -0,0 +1,18 @@
+import { StyleSheet } from 'react-native';
+
+export default StyleSheet.create({
+  'header': {
+    'width': [{ 'unit': '%H', 'value': 1 }],
+    'height': [{ 'unit': 'string', 'value': 'auto' }],
+    'position': 'relative'
+  },
+  'h1_header p_header': {
+    'position': 'absolute',
+    'left': [{ 'unit': '%H', 'value': 0.5 }]
+  },
+  'body': {
+    'margin': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
+    'padding': [{ 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }, { 'unit': 'px', 'value': 0 }],
+    'fontFamily': ''Open Sans', sans-serif'
+  }
+});
